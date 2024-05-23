23157e71fc6ef4ae81160fae0c3325257cc3007a
Adam Ibnu Ridwan <89550112+adam-ibnu-ridwan@users.noreply.github.com>
2024-05-23 18:12:55 +0700

    Uploading index.html and script.js files (Todo List App)

---
+++ script.js
@@ -0,0 +1,60 @@
+// Todo List
+// namaTugas,
+// tambahTugas, hapusTugas(), tandaiSelesai()
+
+class TodoList {
+  constructor(person) {
+    this.person = person;
+    this.tasks = [];
+  }
+
+  addTask(task) {
+    // [C]reate
+    let isSame = this.tasks.find((e) => e.task == task);
+
+    if (isSame == undefined) {
+      this.tasks.push({ task, status: "not cleared" });
+    } else {
+      console.error("This task have created!");
+    }
+  }
+
+  myTasks() {
+    // [R]EAD
+    this.tasks.forEach((e) => {
+      console.log(e);
+    });
+  }
+
+  editTask(task, newTask) {
+    // [U]pdate
+    console.log("...");
+  }
+
+  deleteTask(task) {
+    // [D]elete
+    let newTasks = this.tasks.filter((e) => e.task !== task);
+    if (newTasks.length > 0) {
+      this.tasks = newTasks;
+    } else {
+      console.error("This task is not found!");
+    }
+  }
+
+  markAsCleared(task) {
+    let index = this.tasks.findIndex((e) => e.task == task);
+    if (index > -1) {
+      this.tasks[index].status = "cleared";
+    } else {
+      console.error("This task is not found!");
+    }
+  }
+}
+
+const Adam = new TodoList("Adam");
+Adam.addTask("menghafal juz 10");
+Adam.addTask("menghafal juz 2");
+// Adam.addTask("menghafal juz 15");
+// Adam.markAsCleared("menghafal juz 1");
+// Adam.deleteTask("menghafal juz 15");
+Adam.myTasks();
